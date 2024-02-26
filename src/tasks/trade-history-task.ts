fa01f8f218c774830182928ca0b7056904602ceb
pietro-maximoff <pietro.maxim.official@protonmail.com>
2024-02-26 16:28:55 +0100

    chore: add Ticker

--- src/tasks/trade-history-task.ts
+++ src/tasks/trade-history-task.ts
@@ -3,7 +3,7 @@ import { queryTrades } from '../utils/subgraph'
 import { setLastTaskRun } from '../models/task.model'
 import { TaskType } from '../entity/Task'
 import { tradeSearchCron } from '../crontab'
-import { Trade } from '../entity'
+import { Ticker, Trade } from '../entity'
 import dayjs from 'dayjs'
 
 let lastTimestamp: dayjs.Dayjs | null = null
@@ -24,21 +24,31 @@ export const getTradesTask = async (): Promise<void> => {
     console.log(`getTradesTask() executed. Time since last execution: ${timeDifference} ms`)
 
     const trades = await queryTrades(lastTimestamp, currentTimestamp)
+    console.log('trades', trades)
 
     // Save trades to tradeRepository table
     const tradeRepository = getRepository(Trade)
+    const tickerRepository = getRepository(Ticker)
 
     for (const trade of trades) {
       // Fetch Ticker entities based on token addresses
-      const { timestamp, _return, _amount } = trade
+      const { timestamp, _return, _amount, _fromToken, _toToken } = trade
       const rate = parseFloat((Number(_return) / Number(_amount)).toFixed(9))
+
+      const baseTicker = await tickerRepository.findOne({ address: _toToken.id })
+      const quoteTicker = await tickerRepository.findOne({ address: _fromToken.id })
+
+      console.log('baseTicker', baseTicker)
+      console.log('quoteTicker', quoteTicker)
 
       try {
         await tradeRepository.save({
           date: dayjs.unix(timestamp).toDate(),
           baseAmount: _return,
           quoteAmount: _amount,
-          rate: rate
+          rate: rate,
+          baseTicker: baseTicker,
+          quoteTicker: quoteTicker
         })
       } catch (error) {
         console.error('Error saving trade:', error)
